5913c2ecb1c81971253daf8c1bd98972a58de260
NicolasDabek <nico.dabek@gmail.com>
2024-08-31 10:38:04 +0200

    parameters builder updated

--- src/swagger/builders/parameters.builder.ts
+++ src/swagger/builders/parameters.builder.ts
@@ -3,56 +3,144 @@
 export class ParametersBuilder {
   public static addParametersToRoutes(swaggerDoc: any) {
     Object.keys(swaggerDoc.paths).forEach(path => {
-      // Vérifie si le chemin contient "/:" pour détecter un paramètre de chemin
       if (path.includes('/:')) {
-        const pathParams = path.match(/:([^\/\\*()]+)/g)?.map(param => param.substring(1)); // Extrait tous les noms de paramètres du chemin
-  
-        // Ajouter chaque paramètre de chemin pour les méthodes GET, PUT, DELETE, PATCH
-        ['get', 'put', 'delete', 'patch'].forEach(method => {
-          if (swaggerDoc.paths[path][method]) {
-            swaggerDoc.paths[path][method].parameters = swaggerDoc.paths[path][method].parameters || [];
-            
-            pathParams.forEach(paramName => {
-              swaggerDoc.paths[path][method].parameters.push({
-                name: paramName,
-                in: 'path',
-                required: true,
-                schema: {
-                  type: 'string',
-                },
-                description: `Path parameter '${paramName}' for ${method.toUpperCase()} request`,
-              });
-            });
-          }
-        });
-  
-        // Ajouter toutes les propriétés du body pour les méthodes POST, PUT, PATCH
-        ['post', 'put', 'patch'].forEach(method => {
-          if (swaggerDoc.paths[path][method]) {
-            const properties = swaggerDoc.components.schemas.users.properties; // Assumant que vous avez un schéma 'users'
-            swaggerDoc.paths[path][method].requestBody = {
-              required: true,
-              content: {
-                'application/json': {
+        const paramNames = path.match(/:([^/]+)/g); // Récupère tous les paramètres du chemin
+
+        if (paramNames) {
+          ['get', 'post', 'put', 'patch', 'delete'].forEach(method => {
+            if (swaggerDoc.paths[path][method]) {
+              swaggerDoc.paths[path][method].parameters = swaggerDoc.paths[path][method].parameters || [];
+
+              // Ajoute les paramètres de chemin
+              paramNames.forEach(paramName => {
+                const isInteger = paramName.includes("(\\d+)") || paramName.includes("(\d+)");
+                const paramNameCleaned = paramName.replace(/(\(\\\\d\+\))+|(\(\\d\+\))+|[/:()*+=]/g, '');
+
+                swaggerDoc.paths[path][method].parameters.push({
+                  name: paramNameCleaned,
+                  in: 'path',
+                  required: true,
                   schema: {
-                    type: 'object',
-                    properties,
-                    required: method === 'post' ? Object.keys(properties) : [], // Toutes les propriétés sont required pour POST
+                    type: isInteger ? 'integer' : 'string',
+                  },
+                  description: `${paramNameCleaned} of the resource to ${method}`,
+                });
+              });
+
+              // Ajoute des réponses d'erreur standard
+              swaggerDoc.paths[path][method].responses = {
+                ...swaggerDoc.paths[path][method].responses,
+                '400': {
+                  description: 'Bad Request',
+                  content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/ErrorResponse400',
+                      },
+                    },
                   },
                 },
-              },
-            };
-  
-            // Pour PUT et PATCH, aucun paramètre n'est required mais un body est requis
-            if (method === 'put' || method === 'patch') {
-              swaggerDoc.paths[path][method].requestBody.required = true; // Un body est requis
-              swaggerDoc.paths[path][method].description = `Provide at least one field to update`;
+                '401': {
+                  description: 'Unauthorized',
+                  content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/ErrorResponse401',
+                      },
+                    },
+                  },
+                },
+                '403': {
+                  description: 'Forbidden',
+                  content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/ErrorResponse403',
+                      },
+                    },
+                  },
+                },
+                '404': {
+                  description: 'Not Found',
+                  content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/ErrorResponse404',
+                      },
+                    },
+                  },
+                },
+                '409': {
+                  description: 'Conflict',
+                  content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/ErrorResponse409',
+                      },
+                    },
+                  },
+                },
+                '422': {
+                  description: 'Unprocessable Entity',
+                  content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/ErrorResponse422',
+                      },
+                    },
+                  },
+                },
+                '500': {
+                  description: 'Internal Server Error',
+                  content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/ErrorResponse500',
+                      },
+                    },
+                  },
+                },
+                '503': {
+                  description: 'Service Unavailable',
+                  content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/ErrorResponse503',
+                      },
+                    },
+                  },
+                },
+              };
+
+              // Pour POST, PUT, PATCH, ajoute le body requis
+              if (['post', 'put', 'patch'].includes(method)) {
+                const properties = swaggerDoc.components.schemas.users.properties;
+
+                swaggerDoc.paths[path][method].requestBody = {
+                  required: true,
+                  content: {
+                    'application/json': {
+                      schema: {
+                        type: 'object',
+                        properties,
+                        required: method === 'post' ? Object.keys(properties) : [], // Toutes les propriétés sont required pour POST
+                      },
+                    },
+                  },
+                };
+
+                if (method === 'put' || method === 'patch') {
+                  swaggerDoc.paths[path][method].requestBody.content['application/json'].schema.required = []; // Aucun champ obligatoire
+                  swaggerDoc.paths[path][method].description = `Provide at least one field to update`;
+                }
+              }
             }
-          }
-        });
+          });
+        }
+
       }
     });
-  
+
     return swaggerDoc;
   }
 }
