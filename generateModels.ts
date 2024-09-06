e40e42303aed72a6b4aab3e2dcffdc8976c3dc5f
NicolasDabek <nico.dabek@gmail.com>
2024-09-06 11:20:00 +0200

    generateModels converted to ts

--- generateModels.ts
+++ generateModels.ts
@@ -1,23 +1,28 @@
-require('dotenv').config();
+import { config } from 'dotenv';
+import { exec } from 'child_process';
 
-const { exec } = require('child_process');
+config();
 
+// Construire la commande Sequelize Auto
 let command = `sequelize-auto -h ${process.env.DB_ADDRESS} -d ${process.env.DB_NAME} -u ${process.env.DB_USERNAME} -o ${process.env.DB_DIR_OUTPUT_MODELS} -p ${process.env.DB_PORT} --dialect ${process.env.DB_DIALECT}`;
 
-if(!!process.env.DB_PASSWORD) {
-  command += ` -x ${process.env.DB_PASSWORD}`
+if (process.env.DB_PASSWORD) {
+  command += ` -x ${process.env.DB_PASSWORD}`;
 }
 
-if(!!process.env.DB_MODELS_IN_TYPESCRIPT && process.env.DB_MODELS_IN_TYPESCRIPT === `true`) {
-  command += ` -l ts`
+if (process.env.DB_MODELS_IN_TYPESCRIPT === 'true') {
+  command += ` -l ts`;
 }
 
+// Exécuter la commande
 exec(command, (err, stdout, stderr) => {
   if (err) {
     console.error(`Error: ${err.message}`);
     return;
   }
+
   console.log(`Output: ${stdout}`);
+  
   if (stderr) {
     console.error(`Stderr: ${stderr}`);
   }
