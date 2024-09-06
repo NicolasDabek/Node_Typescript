09166c716072d70157b303c563de55a49a10b466
NicolasDabek <nico.dabek@gmail.com>
2024-09-06 11:26:58 +0200

    unused imports in app.ts removed

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,11 @@ import express from 'express';
 import helmet from 'helmet';
 import hpp from 'hpp';
 import morgan from 'morgan';
-import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import DB from './databases';
 import Routes from './interfaces/routes.interface';
 import errorMiddleware from './middlewares/error.middleware';
 import { logger, stream } from './utils/logger.util';
-import csurf from "csurf"
 import session from "express-session"
 import { Server } from 'http';
 import path from 'path';
