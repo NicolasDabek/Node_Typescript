e82b2a37bde6151743cf5d0b4679c9ae5b723a8e
NicolasDabek <nico.dabek@gmail.com>
2024-09-09 12:11:14 +0200

    base.controller.ts updated

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -1,98 +1,88 @@
-import { Request, Response, NextFunction } from "express"
-import BaseService from "../services/base.service"
-import Model from "sequelize/types/model"
-import { BaseDto } from "../dtos/base.dto"
+import { Request, Response, NextFunction } from "express";
+import BaseService from "../services/base.service";
+import { BaseDto } from "../dtos/base.dto";
+import HttpException from "../exceptions/HttpException";
 
 class BaseController {
-  static baseService = new BaseService()
+  static baseService = new BaseService();
 
   static async getAllDatas(req: Request, res: Response, next: NextFunction) {
     try {
-      const tableName = req.params.model.toString()
-      const findAllDatas: Model[] = await BaseController.baseService.findAllDatas(tableName)
-
-      res.status(200).json({ datas: findAllDatas, message: 'findAll dans getAllDatas' })
+      const tableName = req.params.model.toString();
+      const findAllDatas = await BaseController.baseService.findAllDatas(tableName);
+      res.status(200).json({ datas: findAllDatas, message: 'findAll dans getAllDatas' });
     } catch (error) {
-      next(error)
+      next(error instanceof HttpException ? error : new HttpException(500, 'Internal Server Error'));
     }
   }
 
   static async getDataById(req: Request, res: Response, next: NextFunction) {
     try {
-      const tableName = req.params.model.toString()
-      const dataId = Number(req.params.id)
-      const findOneData: Model = await BaseController.baseService.findDataById(tableName, dataId)
-
-      res.status(200).json({ data: findOneData, message: 'findOne' })
+      const tableName = req.params.model.toString();
+      const dataId = Number(req.params.id);
+      const findOneData = await BaseController.baseService.findDataById(tableName, dataId);
+      res.status(200).json({ data: findOneData, message: 'findOne' });
+    } catch (error) {
+      next(error instanceof HttpException ? error : new HttpException(500, 'Internal Server Error'));
     }
-    catch (error) { next(error) }
   }
 
-  /**
-   * Récupère un seul champ sur toutes les entrées.
-   */
   static async getAllDataOneField(req: Request, res: Response, next: NextFunction) {
     try {
-      const tableName = req.params.model.toString()
-      const fieldName = req.params.fieldName.toString()
-      const findAllDatas: Model[] = await BaseController.baseService.findAllDatasOneField(tableName, fieldName)
-
-      res.status(200).json({ datas: findAllDatas, message: 'findAllDataOneField' })
+      const tableName = req.params.model.toString();
+      const fieldName = req.params.fieldName.toString();
+      const findAllDatas = await BaseController.baseService.findAllDatasOneField(tableName, fieldName);
+      res.status(200).json({ datas: findAllDatas, message: 'findAllDataOneField' });
+    } catch (error) {
+      next(error instanceof HttpException ? error : new HttpException(500, 'Internal Server Error'));
     }
-    catch (error) { next(error) }
   }
 
-  /**
-   * Récupère plusieurs entrées par la valeur d'un champ.
-   */
   static async getMultipleByFieldVal(req: Request, res: Response, next: NextFunction) {
     try {
-      const tableName = req.params.model.toString()
-      const fieldName: string = req.params.fieldName
-      const fieldVal: number = Number(req.params.fieldVal)
-      const findMultipleDatas: Model[] = await BaseController.baseService.findMultipleByFieldName(tableName, fieldName, fieldVal)
-
-      res.status(200).json({ datas: findMultipleDatas, message: 'findMultiple' })
+      const tableName = req.params.model.toString();
+      const fieldName = req.params.fieldName.toString();
+      const fieldVal = Number(req.params.fieldVal);
+      const findMultipleDatas = await BaseController.baseService.findMultipleByFieldName(tableName, fieldName, fieldVal);
+      res.status(200).json({ datas: findMultipleDatas, message: 'findMultiple' });
+    } catch (error) {
+      next(error instanceof HttpException ? error : new HttpException(500, 'Internal Server Error'));
     }
-    catch (error) { next(error) }
   }
 
   static async createData(req: Request, res: Response, next: NextFunction) {
     try {
-      const tableName = req.params.model.toString()
-      const datas: BaseDto = req.body
-      const createdData: Model = await BaseController.baseService.createData(tableName, datas)
-
-      res.status(201).json({ data: createdData, message: 'created' })
+      const tableName = req.params.model.toString();
+      const datas: BaseDto = req.body;
+      const createdData = await BaseController.baseService.createData(tableName, datas);
+      res.status(201).json({ data: createdData, message: 'created' });
     } catch (error) {
-      next(error)
+      next(error instanceof HttpException ? error : new HttpException(500, 'Internal Server Error'));
     }
   }
 
   static async updateData(req: Request, res: Response, next: NextFunction) {
     try {
-      const tableName = req.params.model.toString()
-      const dataId = Number(req.params.id)
-      const datas: BaseDto = req.body
-      const updatedData: Model = await BaseController.baseService.updateData(tableName, dataId, datas)
-
-      res.status(200).json({ data: updatedData, message: 'updated' })
+      const tableName = req.params.model.toString();
+      const dataId = Number(req.params.id);
+      const datas: BaseDto = req.body;
+      const updatedData = await BaseController.baseService.updateData(tableName, dataId, datas);
+      res.status(200).json({ data: updatedData, message: 'updated' });
     } catch (error) {
-      next(error)
+      next(error instanceof HttpException ? error : new HttpException(500, 'Internal Server Error'));
     }
   }
 
   static async deleteData(req: Request, res: Response, next: NextFunction) {
     try {
-      const tableName = req.params.model.toString()
-      const dataId = Number(req.params.id)
-      const deletedData: Model = await BaseController.baseService.deleteData(tableName, dataId)
-
-      res.status(200).json({ data: deletedData, message: 'deleted' })
+      const tableName = req.params.model.toString();
+      const dataId = Number(req.params.id);
+      const deletedData = await BaseController.baseService.deleteData(tableName, dataId);
+      res.status(200).json({ data: deletedData, message: 'deleted' });
     } catch (error) {
-      next(error)
+      next(error instanceof HttpException ? error : new HttpException(500, 'Internal Server Error'));
     }
   }
 }
 
-export default BaseController
+export default BaseController;
