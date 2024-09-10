f49951eba6d34ad2011fe753ece83b40b4c8b8b8
NicolasDabek <nico.dabek@gmail.com>
2024-09-10 13:29:57 +0200

    /src/index.ts updated

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,3 @@ validateEnv()
 export const app = new App(routes)
 
 app.listen()
-
-listRoutes(app)
-function listRoutes(app: App) {
-  app.routes.forEach((currentRoute) => {
-    currentRoute.router.stack.forEach((middleware: any) => {
-    if (middleware.route) { // si c'est une route
-      console.log(`Route: ${Object.keys(middleware.route.methods).join(', ').toUpperCase()} ${middleware.route.path}`);
-    } else if (middleware.name === 'router') { // si c'est un routeur
-      middleware.handle.stack.forEach((handler: any) => {
-        if (handler.route) {
-          console.log(`Route: ${Object.keys(handler.route.methods).join(', ').toUpperCase()} ${handler.route.path}`);
-        }
-      });
-    }
-  });
-  })
-}
