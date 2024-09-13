3d50a21a2b94773636aba4d164371a5e0a77b817
NicolasDabek <nico.dabek@gmail.com>
2024-09-13 16:23:24 +0200

    users service and controllers updated

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,20 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 import UsersService from "../services/users.service";
 import HttpException from "../exceptions/HttpException";
-import { createDtos } from "../dtos";
+import BaseRoute from "../routes/base.route";
 
 class UsersController {
-  static userService = new UsersService();
+  private static userService = new UsersService(BaseRoute.userTable);
 
-  static async registerUser(req: Request, res: Response, next: NextFunction) {
+  public static async registerUser(req: Request, res: Response, next: NextFunction) {
     try {
-      const datas: typeof createDtos.users = req.body;
+      const datas = req.body;
       const createdUser = await this.userService.registerUser(datas);
-      return res.status(200).json({ datas: createdUser });
+      return res.status(201).json({ data: createdUser });
     } catch (error) {
       next(error instanceof HttpException ? error : new HttpException(500, 'Internal Server Error'));
     }
   }
 }
 
-export default UsersController
+export default UsersController;
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,29 +1,40 @@
 import bcrypt from 'bcrypt';
-import { users } from '../models/users';
-import { ObjectUtil } from '../utils/object.util';
+import { Model, ModelStatic, CreationAttributes } from 'sequelize';
 import BaseRoute from '../routes/base.route';
+import { ObjectUtil } from '../utils/object.util';
 
-class UsersService {
-  private userModel = BaseRoute.userTable;
+type UserAttributes<T extends Model> = CreationAttributes<T>;
+
+class UsersService<T extends Model> {
+  private userModel: ModelStatic<T>;
+
+  constructor(userModel: ModelStatic<T>) {
+    this.userModel = userModel;
+  }
 
   private async hashPassword(password: string): Promise<string> {
-    const saltRounds = parseInt(process.env.USER_PASSWORD_HASH_SALT, 10);
+    const saltRounds = parseInt(process.env.USER_PASSWORD_HASH_SALT || '10', 10);
     return bcrypt.hash(password, saltRounds);
   }
 
-  public async registerUser(userDatas: Partial<typeof this.userModel>): Promise<Partial<users>> {
-    userDatas[BaseRoute.fieldNameUserPassword] = await this.hashPassword(
-      userDatas[BaseRoute.fieldNameUserPassword] as string
-    );
+  public async registerUser(userDatas: UserAttributes<T>): Promise<Omit<T, typeof BaseRoute.fieldNameUserPassword>> {
+    const passwordField = BaseRoute.fieldNameUserPassword as keyof UserAttributes<T>;
+    const password = userDatas[passwordField] as string | undefined;
 
-    const createdUser = await this.userModel.create(userDatas);
+    if (!password) {
+      throw new Error('Password is required.');
+    }
+
+    userDatas[passwordField] = await this.hashPassword(password) as any;
+
+    const createdUser = (await this.userModel.create(userDatas)).get({ plain: true });
 
     const userWithoutPasswordField = ObjectUtil.filterKeys(
       createdUser,
-      [BaseRoute.fieldNameUserPassword] as (keyof users)[]
+      [passwordField] as (keyof UserAttributes<T>)[]
     );
 
-    return userWithoutPasswordField;
+    return userWithoutPasswordField as Omit<T, typeof BaseRoute.fieldNameUserPassword>;
   }
 }
 
