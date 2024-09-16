c7dfd152790107590cd8cc6216eb99d7864cc2d3
NicolasDabek <nico.dabek@gmail.com>
2024-09-16 12:31:39 +0200

    base route and service updated

--- src/routes/base.route.ts
+++ src/routes/base.route.ts
@@ -8,31 +8,33 @@ import DB from '../databases'
 import { ClassUtil } from '../utils/class.util'
 
 class BaseRoute implements Route {
-  public path = '/:model'
-  public router = Router()
-  public static userTable = DB.Models.users
+  public path = '/:model';
+  public router = Router();
+  public static userModel = DB.Models.users;
   // Used in BaseService.createData()
-  public static userTableName = ClassUtil.getClassName(this.userTable)
+  public static userModelName = ClassUtil.getClassName(this.userModel);
+  // Used in UsersService.login()
+  public static fieldNameUsername = 'username';
   // Used in BaseService.createData()
-  public static fieldNameUserPassword = "password"
-  public static relTables = []
-  private activateAuthMiddleware = false
-  private checkAuth = (req: RequestWithUser, res: Response, next: NextFunction) => next()
+  public static fieldNameUserPassword = 'password';
+  public static relTables = [];
+  private activateAuthMiddleware = false;
+  private checkAuth = (req: RequestWithUser, res: Response, next: NextFunction) => next();
 
   constructor() {
-    if(this.activateAuthMiddleware) this.checkAuth = authMiddleware
+    if(this.activateAuthMiddleware) this.checkAuth = authMiddleware;
 
-    this.initializeRoutes()
+    this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.checkAuth, BaseControler.getAllDatas)
-    this.router.get(`${this.path}/:id(\\d+)`, this.checkAuth, BaseControler.getDataById)
-    this.router.get(`${this.path}/onefield/:fieldName`, this.checkAuth, BaseControler.getAllDataOneField)
-    this.router.get(`${this.path}/multiple/:fieldName/:fieldVal`, this.checkAuth, BaseControler.getMultipleByFieldVal)
-    this.router.post(`${this.path}`, this.checkAuth, validationMiddleware(), BaseControler.createData)
-    this.router.put(`${this.path}/:id(\\d+)`, this.checkAuth, validationMiddleware('body', true), BaseControler.updateData)
-    this.router.delete(`${this.path}/:id(\\d+)`, this.checkAuth, BaseControler.deleteData)
+    this.router.get(`${this.path}`, this.checkAuth, BaseControler.getAllDatas);
+    this.router.get(`${this.path}/:id(\\d+)`, this.checkAuth, BaseControler.getDataById);
+    this.router.get(`${this.path}/onefield/:fieldName`, this.checkAuth, BaseControler.getAllDataOneField);
+    this.router.get(`${this.path}/multiple/:fieldName/:fieldVal`, this.checkAuth, BaseControler.getMultipleByFieldVal);
+    this.router.post(`${this.path}`, this.checkAuth, validationMiddleware(), BaseControler.createData);
+    this.router.put(`${this.path}/:id(\\d+)`, this.checkAuth, validationMiddleware('body', true), BaseControler.updateData);
+    this.router.delete(`${this.path}/:id(\\d+)`, this.checkAuth, BaseControler.deleteData);
   }
 }
 
--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -2,20 +2,20 @@ import bcrypt from 'bcrypt';
 import DB from '../databases';
 import HttpException from '../exceptions/HttpException';
 import { isEmpty } from '../utils/others.util';
-import { Model } from 'sequelize';
+import { Model, ModelStatic } from 'sequelize';
 import BaseRoute from '../routes/base.route';
 
-class BaseService {
+class BaseService<T extends Model> {
   private models = DB.Models;
 
-  public async findAllDatas(tableName: string): Promise<Model[]> {
-    if (isEmpty(tableName)) throw new HttpException(400, "Table name is required.");
-    return await this.models[tableName]?.findAll();
+  public async findAllDatas(modelName: string): Promise<Model[]> {
+    if (isEmpty(modelName)) throw new HttpException(400, "Table name is required.");
+    return await this.models[modelName]?.findAll();
   }
 
-  public async findDataById(tableName: string, dataId: number): Promise<Model> {
+  public async findDataById(modelName: string, dataId: number): Promise<Model> {
     if (isNaN(dataId)) throw new HttpException(400, "Valid data ID is required.");
-    const findData: Model = await this.models[tableName]?.findByPk(dataId);
+    const findData: Model = await this.models[modelName]?.findByPk(dataId);
     if (!findData) throw new HttpException(404, "Data not found.");
     return findData;
   }
@@ -23,26 +23,26 @@ class BaseService {
   /**
    * Récupère un seul champ sur toutes les entrées.
    */
-  public async findAllDatasOneField(tableName: string, fieldName: string): Promise<Model[]> {
+  public async findAllDatasOneField(modelName: string, fieldName: string): Promise<Model[]> {
     const options: Object = { where: { deleted: 0 }, attributes: ["id", fieldName] };
-    return await this.models[tableName]?.findAll(options);
+    return await this.models[modelName]?.findAll(options);
   }
 
   /**
    * Récupère plusieurs entrées par la valeur d'un champ.
    */
-  public async findMultipleByFieldName(tableName: string, fieldName: string, fieldVal: number): Promise<Model[]> {
+  public async findMultipleByFieldName(modelName: string, fieldName: string, fieldVal: string | number): Promise<T[]> {
     if (isEmpty(fieldName) || isEmpty(fieldVal)) throw new HttpException(400, "Field name and value are required.");
     const options: Object = { where: { [fieldName]: fieldVal } };
-    const rows = await this.models[tableName]?.findAll(options);
+    const rows = (await this.models[modelName] as ModelStatic<T>)?.findAll(options);
     if (!rows) throw new HttpException(404, "No rows found.");
     return rows;
   }
 
-  public async findLastData(tableName: string): Promise<Model> {
+  public async findLastData(modelName: string): Promise<Model> {
     try {
       const options: Object = { order: [['id', 'DESC']], limit: 1 };
-      const lastData = await this.models[tableName]?.findOne(options);
+      const lastData = await this.models[modelName]?.findOne(options);
       if (!lastData) throw new HttpException(404, "No data found.");
       return lastData;
     } catch (error) {
@@ -50,31 +50,31 @@ class BaseService {
     }
   };
 
-  public async createData(tableName: string, datas: Model): Promise<Model> {
+  public async createData(modelName: string, datas: Model): Promise<Model> {
     if (isEmpty(datas)) throw new HttpException(400, "Data is required.");
-    return await this.models[tableName]?.create(datas);
+    return await this.models[modelName]?.create(datas);
   }
 
-  public async updateData(tableName: string, dataId: number, datas: Model): Promise<Model> {
+  public async updateData(modelName: string, dataId: number, datas: Model): Promise<Model> {
     if (isEmpty(datas)) throw new HttpException(400, "Data is required.");
-    const findData: Model = await this.models[tableName].findByPk(dataId);
+    const findData: Model = await this.models[modelName].findByPk(dataId);
     if (!findData) throw new HttpException(404, "Data not found.");
 
-    if (BaseRoute.userTableName == tableName && datas[BaseRoute.fieldNameUserPassword]) {
+    if (BaseRoute.userModelName == modelName && datas[BaseRoute.fieldNameUserPassword]) {
       datas[BaseRoute.fieldNameUserPassword] = await bcrypt.hash(
         datas[BaseRoute.fieldNameUserPassword],
         parseInt(process.env.USER_PASSWORD_HASH_SALT, 10)
       );
     }
-    await this.models[tableName].update(datas, { where: { id: dataId } });
-    return await this.models[tableName]?.findByPk(dataId);
+    await this.models[modelName].update(datas, { where: { id: dataId } });
+    return await this.models[modelName]?.findByPk(dataId);
   }
 
-  public async deleteData(tableName: string, dataId: number): Promise<Model> {
+  public async deleteData(modelName: string, dataId: number): Promise<Model> {
     if (isNaN(dataId)) throw new HttpException(400, "Valid data ID is required.");
-    const findData: Model = await this.models[tableName].findByPk(dataId);
+    const findData: Model = await this.models[modelName].findByPk(dataId);
     if (!findData) throw new HttpException(404, "Data not found.");
-    await this.models[tableName]?.destroy({ where: { id: dataId } });
+    await this.models[modelName]?.destroy({ where: { id: dataId } });
     return findData;
   }
 }
