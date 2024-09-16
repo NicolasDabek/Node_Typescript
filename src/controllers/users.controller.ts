8274d2b85d4667e94de8d7b8987edce89139b524
NicolasDabek <nico.dabek@gmail.com>
2024-09-16 16:12:11 +0200

    users controller and service updated

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -13,22 +13,24 @@ class UsersController<T extends Model> {
   public registerUser = async (req: Request<{}, {}, Partial<CreationAttributes<T>>>, res: Response, next: NextFunction) => {
     try {
       const datas = req.body;
+      if (!datas) throw new HttpException(400, 'User data is required');
       const createdUser = await this.userService.registerUser(datas);
       return res.status(201).json({ datas: createdUser });
     } catch (error) {
       next(error instanceof HttpException ? error : new HttpException(500, 'Internal Server Error'));
     }
-  };
+  }
 
   public loginUser = async (req: Request<{}, {}, Partial<CreationAttributes<T>>>, res: Response, next: NextFunction) => {
     try {
       const { username, password } = req.body;
-      const token = await this.userService.login(username, password);
+      if (!username || !password) throw new HttpException(400, 'Username and password are required');
+      const token = await this.userService.loginUser(username, password);
       return res.status(200).json({ datas : token });
     } catch (error) {
       next(error instanceof HttpException ? error : new HttpException(500, 'Internal Server Error'));
     }
-  };
+  }
 }
 
 export default UsersController;
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -22,31 +22,23 @@ class UsersService<T extends Model<typeof BaseRoute.userModel>> {
 
   private async validatePassword(user: Partial<T>, password: string): Promise<boolean> {
     const isPasswordValid = await bcrypt.compare(password, user[BaseRoute.fieldNameUserPassword]);
-    if (!isPasswordValid) {
-      throw new HttpException(401, 'Invalid email or password');
-    }
+    if (!isPasswordValid) throw new HttpException(401, 'Invalid email or password');
     return true;
   }
 
   public async registerUser(userDatas: Partial<CreationAttributes<T>>): Promise<Partial<T>> {
     const passwordField = BaseRoute.fieldNameUserPassword as keyof T;
     const password = userDatas[passwordField as string] as string | undefined;
-
     if (!password) throw new Error('Password is required.');
-
     userDatas[passwordField as string] = await this.hashPassword(password);
-
     const createdUser = await this.userModel.create(userDatas as CreationAttributes<T>);
     return ObjectUtil.filterKeys(createdUser, [passwordField]);
   }
 
-  public async login(username: string, password: string): Promise<string> {
+  public async loginUser(username: string, password: string): Promise<string> {
+    if (!username || !password) throw new HttpException(400, 'Username and password are required');
     const user = await this.baseService.findMultipleByFieldName(BaseRoute.userModelName, BaseRoute.fieldNameUsername, username);
-
-    if (!user || user.length === 0) {
-      throw new HttpException(401, 'User not found');
-    }
-
+    if (!user || user.length === 0) throw new HttpException(401, 'User not found');
     await this.validatePassword(user[0], password);
     const token = jwt.sign({ username: user[0][BaseRoute.fieldNameUsername] }, process.env.JWT_SECRET || 'default_secret', { expiresIn: '1h' });
     return token;
