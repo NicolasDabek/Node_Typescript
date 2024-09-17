5bd832b2c693474367a2764600d43797fc4e7bc3
NicolasDabek <nico.dabek@gmail.com>
2024-09-17 09:36:48 +0200

    error.middleware updated

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -6,10 +6,16 @@ const errorMiddleware = (error: HttpException, req: Request, res: Response, next
   try {
     const status: number = error.status || 500;
     const message: string = error.message || 'Something went wrong';
-    logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
-    res.status(status).json({ message });
-  } catch (error) {
-    next(error);
+    logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}, Headers:: ${JSON.stringify(req.headers)}, Body:: ${JSON.stringify(req.body)}`);
+
+    if (process.env.NODE_ENV === 'development') {
+      res.status(status).json({ message, stack: error.stack });
+    } else {
+      res.status(status).json({ message });
+    }
+  } catch (loggingError) {
+    logger.error('Error handling error: ', loggingError);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
